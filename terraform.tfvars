927ce21507e10c9a04107de9317ee324db8671f9
Alisson Allebrandt <alisson.allebrandtt@gmail.com>
2022-07-26 22:28:05 -0300

    Initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+prefix = "fullcycle"
+cluster_name = "clusterfc"
+retention_days = 1
+desired_size = 2
+max_size = 4
+min_size = 2
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "cluster_name" {}
+variable "retention_days" {}
+variable "desired_size" {}
+variable "max_size" {}
+variable "min_size" {}
